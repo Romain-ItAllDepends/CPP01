85a9be0fccdd1c4c54f99e63b9ce34c984e0f9a9
Romain Gobet <rgobet@1G5.42angouleme.fr>
2024-07-30 14:56:54 +0200

    Perfect

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rgobet <rgobet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 13:58:20 by rgobet            #+#    #+#             */
-/*   Updated: 2024/07/30 14:34:19 by rgobet           ###   ########.fr       */
+/*   Updated: 2024/07/30 14:55:37 by rgobet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,25 @@
 int	main(void) {
 	Harl		harl;
 	std::string	in;
+	int			i;
 
-	std::cin >> in;
-	harl.complain(in);
-	std::cin.clear();
+	i = 0;
+	while (42) {
+		std::cout << "Please enter a level of incident > ";
+		while (!(std::cin >> in))
+			std::cout << std::endl << "Please enter a level of incident > ";
+		harl.complain(in);
+		if (i % 3 == 0) {
+			do {
+				std::cin.clear();
+				std::cout << "Do you want to continue ? (y or n) > ";
+				std::cin >> in;
+			} while (in.empty() == false && in != "y" && in != "n");
+			std::cin.clear();
+			if (in == "n")
+				return (0);
+		}
+		i++;
+	}
 	return (0);
 }
